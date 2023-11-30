ec9872480502145ffdfdcdabb4e7ec8da303f190
01omer <omer00091@outlook.com>
2023-11-30 10:17:43 -0800

    done

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
+
 def magic_calculation(a, b):
-    result = 98
-    result += a ** b
-    return result
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
