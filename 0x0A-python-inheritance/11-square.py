e4e7464db011664dcc6b61176b318cf5c054e3f6
01omer <omer00091@outlook.com>
2024-01-08 12:01:21 -0800

    done

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,6 @@
- a Rectangle subclass Square."""
+#!/usr/bin/python3
+"""Defines a base geometry class Rectangle."""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -12,5 +14,13 @@ class Square(Rectangle):
             size (int): The size of the new square.
         """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """informal string reepresentation of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
